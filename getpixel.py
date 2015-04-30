4c26a44ba6e6559c1f1db93ebc23c91b34f43ae1
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-05-01 00:58:36 +0100

    changed getpixel to work by blocks instead of individual pixels

--- getpixel.py
+++ getpixel.py
@@ -10,13 +10,19 @@ except FileNotFoundError:
 print(im.format, im.size, im.mode)
 
 width, height = im.size
+blockSize = 30;
 
 out = Image.new('RGB', (width, height), 'black')
 
-for i in range(1000000):
+for i in range(500):
     w = random.randrange(width)
     h = random.randrange(height)
-    out.putpixel((w,h), im.getpixel((w,h)))
+    for i in range(blockSize):
+    	for j in range(blockSize):
+   			try:
+   				out.putpixel((w+i,h+j), im.getpixel((w+i,h+j)))
+   			except IndexError:
+   				break
 
 #im.show()
 out.show()
